40af0535fa01d1a2ae4b6896e1d22897c42ebd1c
JerryYin <846597629@qq.com>
2016-09-20 10:11:15 +0800

    added a button

--- app/src/main/java/com/greentech/ansibledt/materialdemo/activity/InfoActivity.java
+++ app/src/main/java/com/greentech/ansibledt/materialdemo/activity/InfoActivity.java
@@ -1,17 +1,23 @@
 package com.greentech.ansibledt.materialdemo.activity;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 import com.greentech.ansibledt.materialdemo.R;
 
 public class InfoActivity extends AppCompatActivity {
 
+//    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ayout_info);
+
+//        this.setActionBar(new Toolbar(this));
     }
 
 
